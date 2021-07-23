8662141a42024a63c2ccacb78a578a705bc9f4c7
uioz <36815772+uioz@users.noreply.github.com>
2021-07-23 15:43:40 +0800

    Update constants.js

--- build/constants.js
+++ build/constants.js
@@ -3,8 +3,8 @@ const path = require("path");
 exports.CONTEXT = path.join(__dirname, "..");
 
 exports.OUTPUT_DIR = "./dist";
-exports.OUTPUT_PUBLIC_PATH = "/";
-exports.DEVSERVER_PUBLIC_PATH = "http://localhost:8080/";
+exports.OUTPUT_PUBLIC_PATH = "http://localhost:8080/";
+exports.DEVSERVER_PUBLIC_PATH = "/";
 exports.DEVSERVER_PORT = 8080;
 exports.DEVSERVER_HOST = "localhost";
 exports.DEVSERVER_CONTENT_BASE = "static";
